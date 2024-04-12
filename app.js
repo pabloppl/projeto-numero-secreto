9f24a058f43ca7caca60bb5425759fe75844d4cf
Pablo Pereira <pablopereira444@hotmail.com>
2024-04-12 17:17:15 -0300

    Atualizar valor máximo de 10 pra 100

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 //titulo.innerHTML = 'Jogo do número secreto';
 
 //let paragrafo = document.querySelector('p');
-//titulo.innerHTML = 'Escolha um número entre 1 a 10.';
+//titulo.innerHTML = 'Escolha um número entre 1 a 100.';
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -16,7 +16,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
 exibirTextoNaTela('h1', 'Bem vindo ao jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 a 10.');
+exibirTextoNaTela('p', 'Escolha um número entre 1 a 100.');
 }
 
 exibirMensagemInicial();
